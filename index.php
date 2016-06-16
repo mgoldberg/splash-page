f03a2f4d306a86526eb777918f6def3d197702d5
Mark Goldberg <mgoldberg@launchdm.com>
2016-06-16 08:47:02 -0400

    finished setting up

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+
+
+	<head>
+		<link type="text/css" rel="stylesheet" href="stylesheet.css"/>
+		<title>Splash Page</title>
+	</head>
+
+
+
+	<body>
+		<header>
+			qefasdgasdf
+		</header>
+	</body>
+
+</HTMl>	
